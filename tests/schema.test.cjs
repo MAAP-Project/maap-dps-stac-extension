bd38dc03041c6246abff2d5dce66a9cd200f2674
Henry Rodman <henry.rodman@gmail.com>
2026-09-15 20:11:34 -0500

    chore: prepare v0.1.0 release (#5)

--- tests/schema.test.cjs
+++ tests/schema.test.cjs
@@ -1,8 +1,27 @@
 const assert = require('node:assert/strict');
+const { readFileSync } = require('node:fs');
 const { test } = require('node:test');
 const Ajv = require('ajv');
+const packageJson = require('../package.json');
 const schema = require('../json-schema/schema.json');
 const example = require('../examples/item.json');
+
+const schemaUrl = `https://maap-project.github.io/maap-dps-stac-extension/v${packageJson.version}/schema.json`;
+
+test('published references use the package version', () => {
+  assert.equal(schema.$id, schemaUrl);
+  assert.equal(schema.properties.stac_extensions.allOf[0].contains.const, schemaUrl);
+  assert.ok(example.stac_extensions.includes(schemaUrl));
+
+  for (const script of ['check-examples', 'format-examples']) {
+    assert.ok(packageJson.scripts[script].includes(schemaUrl));
+  }
+
+  const readme = readFileSync('README.md', 'utf8');
+  assert.ok(readme.includes(`Version ${packageJson.version} applies`));
+  assert.ok(readme.includes(`version ${packageJson.version}.`));
+  assert.equal(readme.split(schemaUrl).length - 1, 2);
+});
 
 test('DPS requires the processing version and extension declaration', () => {
   const validate = new Ajv({ strict: false }).compile(schema);
